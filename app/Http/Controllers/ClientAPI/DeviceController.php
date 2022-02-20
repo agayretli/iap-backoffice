50ce1127fc15c135da76c0a56b083d9e37dc990c
agayretli <abdurrahmangayretli@gmail.com>
2022-02-20 14:31:32 +0300

    init

---
+++ app/Http/Controllers/ClientAPI/DeviceController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\ClientAPI;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeviceController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function register(Request $request)
+    {
+        //todo
+    }
+
+    public function purchase(Request $request)
+    {
+        //todo
+    }
+
+    public function checkSubscription(Request $request)
+    {
+        //todo
+    }
+}
