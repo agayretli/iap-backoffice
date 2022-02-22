346364eea52b2dbfa9ae9ae99f5d3cb3fe8437db
agayretli <abdurrahmangayretli@gmail.com>
2022-02-22 13:13:05 +0300

    added mock side

---
+++ app/Http/Controllers/Validation/ValidateController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Validation;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+
+class ValidateController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function googleVerify($receipt)
+    {
+        return $this->mock($receipt);
+    }
+
+    public function iosVerify($receipt)
+    {
+        return mock($receipt);
+    }
+
+    public function mock($receipt)
+    {
+        $response = [];
+        // Rate-limit error
+        if (!(($receipt % 100) % 6)) {
+            if (rand(0, 1)) {
+                //todo
+                //$response['message'] = 'RATE-LIMIT ERROR';
+            }
+        }
+
+        //validate
+        if ($receipt % 2) {
+            $response['message'] = 'OK';
+            $response['status'] = true;
+            $date = Carbon::now()->addMonth();
+            $response['expire-date'] = $date->setTimezone('America/Belize')->format('Y-m-d H:i:s');
+
+            return $response;
+        }
+        $response['message'] = 'REJECT';
+        $response['status'] = false;
+
+        return $response;
+    }
+}
--- app/Jobs/CheckDeviceAppSubscription.php
+++ app/Jobs/CheckDeviceAppSubscription.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\Events\Canceled;
+use App\Events\Renewed;
 use App\Models\DeviceApp;
 use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
@@ -44,10 +45,21 @@ class CheckDeviceAppSubscription implements ShouldQueue
         $expire_date = $this->device_app->expire_date;
 
         if (Carbon::now()->gt($expire_date)) {
-            //todo mocking
-            $this->device_app->expire_date = null;
-            $this->device_app->subscription = 0;
-            event(new Canceled($device_app));
+            //Validate Receipt
+            if ($this->device_app->operating_system == 'android') {
+                $response = app('App\Http\Controllers\Validation\ValidateController')->googleVerify($this->device_app->receipt);
+            } else {
+                $response = app('App\Http\Controllers\Validation\ValidateController')->iosVerify($this->device_app->receipt);
+            }
+            if ($response['status']) {
+                $this->device_app->expire_date = $response['expire-date'];
+                event(new Renewed($this->device_app));
+            } else {
+                $this->device_app->expire_date = null;
+                $this->device_app->subscription = false;
+                $this->device_app->receipt = null;
+                event(new Canceled($this->device_app));
+            }
         }
         $this->device_app->save();
     }
